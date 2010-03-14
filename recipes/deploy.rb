20d2578f65f0231143b9f3f6909ac32ad7220702
Joost Baaij <joost@spacebabies.nl>
2010-03-14 20:55:58 +0100

    first commit, extracted from Z@PP

---
+++ recipes/deploy.rb
@@ -0,0 +1,22 @@
+set(:db_remote) do
+  db_config = capture "cat /e/ap/#{application}/config/database.yml"
+  YAML::load(db_config)['production']
+end
+
+set(:db_local) do
+  YAML::load_file("config/database.yml")['development']
+end
+
+desc 'Fetch the remote production database and overwrite your local development database with it'
+namespace :db do
+  task :fetch, :roles => :db do
+    dumpfile = "/e/ap/#{application}/tmp/#{db_remote['database']}.sql"
+    run "mysqldump --opt -u #{db_remote['username']} --password=#{db_remote['password']} -h #{db_remote['host']} #{db_remote['database']} > #{dumpfile}"
+
+    system "rsync -vP #{user}@#{deploy_host}:#{dumpfile} tmp/#{db_local["database"]}.sql"
+    system "mysql -u #{db_local['username']} --password=#{db_local['password']} #{db_local['database']} < tmp/#{db_local["database"]}.sql"
+
+    run "rm #{dumpfile}"
+    system "rm tmp/#{db_local["database"]}.sql"
+  end
+end
---
+++ tasks/capistrano_database_tasks_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :capistrano_database_tasks do
+#   # Task goes here
+# end
