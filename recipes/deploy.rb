ba6e8d1229f87d5f061b2c1d1ff4600718a634fc
Joost Baaij <joost@spacebabies.nl>
2010-03-20 20:37:26 +0100

    namespaced the commands to prevent collisions

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,29 +1,31 @@
-namespace :fetch do
-  desc 'Fetch the remote production database and overwrite your local development database with it'
-  task :db, :roles => :db do
-    run "mysqldump --opt -u #{db_remote['username']} --password=#{db_remote['password']} -h #{db_remote['host']} #{db_remote['database']} > #{dumpfile}"
+namespace :transmit do
+  namespace :get do
+    desc 'Fetch the remote production database and overwrite your local development database with it'
+    task :db, :roles => :db do
+      run "mysqldump --opt -u #{db_remote['username']} --password=#{db_remote['password']} -h #{db_remote['host']} #{db_remote['database']} > #{dumpfile}"
 
-    system "rsync -vP #{user}@#{deploy_host}:#{dumpfile} tmp/#{db_local["database"]}.sql"
-    system "mysql -u #{db_local['username']} --password=#{db_local['password']} #{db_local['database']} < tmp/#{db_local["database"]}.sql"
+      system "rsync -vP #{user}@#{deploy_host}:#{dumpfile} tmp/#{db_local["database"]}.sql"
+      system "mysql -u #{db_local['username']} --password=#{db_local['password']} #{db_local['database']} < tmp/#{db_local["database"]}.sql"
 
-    run "rm #{dumpfile}"
-    system "rm tmp/#{db_local["database"]}.sql"
+      run "rm #{dumpfile}"
+      system "rm tmp/#{db_local["database"]}.sql"
+    end
+
+    desc 'Fetch the assets from the production server to the development environment'
+    task :assets, :roles => :app do
+      system "rsync -Lcrvz #{user}@#{deploy_host}:#{current_path}/public ."
+    end
   end
 
-  desc 'Fetch the assets from the production server to the development environment'
-  task :assets, :roles => :app do
-    system "rsync -Lcrvz #{user}@#{deploy_host}:#{current_path}/public ."
-  end
-end
-
-namespace :put do
-  desc 'Upload the local development database to the remote production database and overwrite it'
-  task :db, :roles => :db do
-    system "mysqldump --opt -u #{db_local['username']} --password=#{db_local['password']} -h #{db_local['host']} #{db_local['database']} > tmp/#{db_local['database']}.sql"
-    system "rsync -vP tmp/#{db_local['database']}.sql #{user}@#{deploy_host}:#{dumpfile}"
-    run "mysql -u #{db_local['username']} --password=#{db_local['password']} #{db_local['database']} < #{dumpfile}"
-    run "rm #{dumpfile}"
-    system "rm tmp/#{db_local['database']}.sql"
+  namespace :put do
+    desc 'Upload the local development database to the remote production database and overwrite it'
+    task :db, :roles => :db do
+      system "mysqldump --opt -u #{db_local['username']} --password=#{db_local['password']} -h #{db_local['host']} #{db_local['database']} > tmp/#{db_local['database']}.sql"
+      system "rsync -vP tmp/#{db_local['database']}.sql #{user}@#{deploy_host}:#{dumpfile}"
+      run "mysql -u #{db_local['username']} --password=#{db_local['password']} #{db_local['database']} < #{dumpfile}"
+      run "rm #{dumpfile}"
+      system "rm tmp/#{db_local['database']}.sql"
+    end
   end
 end
 
