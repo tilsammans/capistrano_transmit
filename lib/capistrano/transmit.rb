7a2e0d04444f82e286a9e91fb46fb5e2cbcf03a8
Joost Baaij <joost@spacebabies.nl>
2011-08-04 14:11:46 +0200

    make user an optional setting

--- lib/capistrano/transmit.rb
+++ lib/capistrano/transmit.rb
@@ -4,14 +4,19 @@ end
 
 Capistrano::Configuration.instance.load do
   
-  _cset :user,      ""
-  _cset :db_config, { capture("cat #{current_path}/config/database.yml") }
-  _cset :db_remote, { YAML::load(db_config)['production'] }
-  _cset :db_local,  { YAML::load_file("config/database.yml")['development'] }
-  _cset :dumpfile,  { "#{current_path}/tmp/#{db_remote['database']}.sql.gz" }
+  _cset :user,      nil
+  _cset(:db_config) { capture("cat #{current_path}/config/database.yml") }
+  _cset(:db_remote) { YAML::load(db_config)['production'] }
+  _cset(:db_local)  { YAML::load_file("config/database.yml")['development'] }
+  _cset(:dumpfile)  { "#{current_path}/tmp/#{db_remote['database']}.sql.gz" }
   
   after "transmit:get:mysql", "transmit:cleanup"
   after "transmit:put:mysql", "transmit:cleanup"
+
+  # Return just the deploy host when user not set.  
+  def _user_with_host
+    user.nil? ? deploy_host : "#{user}@#{deploy_host}"
+  end
 
   namespace :transmit do
     namespace :get do
@@ -19,13 +24,13 @@ Capistrano::Configuration.instance.load do
       task :mysql, :roles => :db do
         run "mysqldump --opt --quick --extended-insert --skip-lock-tables -u #{db_remote['username']} --password='#{db_remote['password']}' --host='#{db_remote['host']}' #{db_remote['database']} | gzip > #{dumpfile}"
 
-        system "rsync -vP #{user}@#{deploy_host}:#{dumpfile} tmp/#{db_local["database"]}.sql.gz"
+        system "rsync -vP #{_user_with_host}:#{dumpfile} tmp/#{db_local["database"]}.sql.gz"
         system "gunzip < tmp/#{db_local["database"]}.sql.gz | mysql -u #{db_local['username']} --password='#{db_local['password']}' --host='#{db_local['host']}' #{db_local['database']}"
       end
 
       desc 'Fetch the assets from the production server to the development environment'
       task :assets, :roles => :app do
-        system "rsync -Lcrvz #{user}@#{deploy_host}:#{current_path}/public ."
+        system "rsync -Lcrvz #{_user_with_host}:#{current_path}/public ."
       end
     end
 
@@ -34,7 +39,7 @@ Capistrano::Configuration.instance.load do
       task :mysql, :roles => :db do
         system "mysqldump --opt -u #{db_local['username']} --password='#{db_local['password']}' --host='#{db_local['host']}' #{db_local['database']} > tmp/#{db_local['database']}.sql"
 
-        system "rsync -vP tmp/#{db_local['database']}.sql #{user}@#{deploy_host}:#{dumpfile}"
+        system "rsync -vP tmp/#{db_local['database']}.sql #{_user_with_host}:#{dumpfile}"
         run "mysql -u #{db_remote['username']} --password='#{db_remote['password']}' --host='#{db_remote['host']}' #{db_remote['database']} < #{dumpfile}"
       end
     end
